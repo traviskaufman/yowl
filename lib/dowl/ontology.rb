175c1673629a12b0d4fd7a827748877147f72cad
Jacobus Geluk <jacobus.geluk@gmail.com>
2012-08-17 11:49:23 -0400

    More support for alternative names

--- lib/dowl/ontology.rb
+++ lib/dowl/ontology.rb
@@ -13,30 +13,61 @@ module DOWL
     end
     
     def title()
-      return get_literal(DOWL::Namespaces::DCTERMS.title)
+      dctermsTitle = get_literal(DOWL::Namespaces::DCTERMS.title)
+      if dctermsTitle
+        return dctermsTitle
+      end 
+      return get_literal(DOWL::Namespaces::DC.title)
     end
     
     def created()
-      return get_literal(DOWL::Namespaces::DCTERMS.created)
+      dctermsCreated = get_literal(DOWL::Namespaces::DCTERMS.created)
+      if dctermsCreated
+        return dctermsCreated
+      end
+      return get_literal(DOWL::Namespaces::DC.date)
     end
 
     def modified()
       return get_literal(DOWL::Namespaces::DCTERMS.modified)
     end
     
+    def rights()
+      return get_literal(DOWL::Namespaces::DC.rights)
+    end
+    
     def authors()      
       authors = []
+      #
+      # First find the Authors by searching for foaf:maker
+      #
       @schema.model.query( 
-        RDF::Query::Pattern.new( @resource, DOWL::Namespaces::FOAF.maker ) ) do |statement|
-          authors << Person.new( statement.object, @schema )
+        RDF::Query::Pattern.new(@resource, DOWL::Namespaces::FOAF.maker)
+      ) do |statement|
+        authors << Person.new(statement.object, @schema)
+      end
+      #
+      # Also scan for dc:creator and dc:contributor
+      #         
+      @schema.model.query( 
+        RDF::Query::Pattern.new(@resource, DOWL::Namespaces::DC.creator)
+      ) do |statement|
+          person = Person.new( nil, @schema )
+          person.setName(statement.object)
+          authors << person
       end         
       @schema.model.query( 
-        RDF::Query::Pattern.new( @resource, DOWL::Namespaces::DC.creator ) ) do |statement|
+        RDF::Query::Pattern.new(@resource, DOWL::Namespaces::DC.contributor)
+      ) do |statement|
           person = Person.new( nil, @schema )
           person.setName(statement.object)
           authors << person
       end         
       return authors.sort     
+    end
+    
+    def hasAuthors()
+      return authors.empty?
     end
     
   end

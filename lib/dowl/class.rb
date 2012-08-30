af47824a66d56fb1480a22de95ef39be5921da6f
Jacobus Geluk <jacobus.geluk@gmail.com>
2012-08-30 13:03:05 -0400

    Moved class Association to its own file

--- lib/dowl/class.rb
+++ lib/dowl/class.rb
@@ -177,32 +177,4 @@ module DOWL
 
   end
 
-  class Association < DOWL::LabelledDocObject
-
-    attr_reader :domainClass
-    attr_reader :rangeClass
-    attr_reader :property
-    def initialize(resource, schema, domainClass, rangeClass)
-      super(resource, schema)
-      @domainClass = domainClass
-      @rangeClass = rangeClass
-    end
-
-    def hash
-      "#{@domainClass.uri},#{@rangeClass.uri},#{@propery}".hash
-    end
-
-    def label
-      return short_name
-    end
-    
-    def addAsGraphVizEdge(graph, nodes)
-      
-      domainClassNode = nodes[@domainClass.uri]
-      rangeClassNode = nodes[@rangeClass.uri]
-        
-      graph.add_edges(domainClassNode, rangeClassNode, :label => label)
-    end
-  end
-
 end

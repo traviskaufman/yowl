0e0819b6c17d914ef5ecef40491f7365e3afd1a2
Jacobus Geluk <jacobus.geluk@gmail.com>
2012-08-31 12:40:49 -0400

    Minor Changes

--- lib/dowl/schema.rb
+++ lib/dowl/schema.rb
@@ -290,7 +290,7 @@ module DOWL
             if @options.verbose
               puts "  - Processing super class #{superClass.short_name}"
             end
-            g.add_edges(nodes[klass.uri], superClassNode, :xlabel => "subClassOf", :arrowHead => :empty, :style => :bold)
+            g.add_edges(nodes[klass.uri], superClassNode, :xlabel => "subClassOf", :arrowhead => :empty, :style => :bold)
           else
             if @options.verbose
               puts "  - Processing super class #{superClass.short_name}, not found"
